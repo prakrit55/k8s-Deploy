00c9378ebd996143ff1d10333eaef9ed36f46d24
Prakriti Mandal <prakritimandal611@gmail.com>
2025-10-07 12:29:54 +0530

    Changed: Configured workflow

--- app/products/page.tsx
+++ app/products/page.tsx
@@ -1,12 +1,8 @@
-import { Product } from "@/mocks/products";
+import { mockProducts, Product } from "@/mocks/products";
 import Link from "next/link";
 
-export default async function ProductsPage() {
-  const resp = await fetch("http://localhost:3000/api/products", {
-    // Alternatively, can set via https://nextjs.org/docs/app/api-reference/file-conventions/route-segment-config#revalidate
-    next: { revalidate: 60 }, // Revalidate every 60 seconds
-  });
-  const products = (await resp.json()) as Product[];
+export default function ProductsPage() {
+  const products: Product[] = mockProducts; // Directly use mock data
 
   return (
     <>
